cdc712a074705c4b0a7daa7d1faabbdfef13c361
ls <1498852016@qq.com>
2021-05-29 19:41:33 +0800

    提交项目

---
+++ aj/WebContent/js/ajax.js
@@ -0,0 +1,16 @@
+function getXhr(){
+	var xhr=null;
+	if(window.XMLHttpRequest){
+		xhr=new XMLHttpRequest();
+	}else{
+		xhr=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	return xhr;
+}
+function $(id) {
+	return document.getElementById(id);
+}
+//依据id查找节点
+function $F(id) {
+	return $(id).value;
+}
